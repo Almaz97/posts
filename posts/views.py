4a613e982756b75aab39235382634ed42ff94297
Almaz97 <almaz.carato@gmail.com>
2022-04-11 18:03:51 +0600

    Add posts like/unlike API for users

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,7 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework import status
 
 from .models import Post
 from .serializers import PostSerializer
@@ -8,4 +11,14 @@ class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    @action(detail=True, methods=["POST"])
+    def like(self, request, pk):
+        post = self.get_object()
+        post.likes.add(request.user)
+        return Response(status=status.HTTP_200_OK)
 
+    @action(detail=True, methods=["POST"])
+    def unlike(self, request, pk):
+        post = self.get_object()
+        post.likes.remove(request.user)
+        return Response(status=status.HTTP_200_OK)
